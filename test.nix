e161cf0b717506370af7026d0d030551b9e12100
Kai Harries <kai.harries@posteo.de>
2025-11-22 17:15:48 +0100

    Start http-server before test execution

--- test.nix
+++ test.nix
@@ -8,6 +8,8 @@
     shellHook = ''
       export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
       export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+      python3 -m http.server 3000 &> http-server.log &
+      trap "kill %1" EXIT
       npm install @playwright/test@1.52.0
       npx playwright@1.52.0 test
     '';
