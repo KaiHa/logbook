e1db7d997c725d649f43c79d53f67d639392a311
Kai Harries <kai.harries@posteo.de>
2025-12-15 17:44:01 +0100

    Update playwright

--- test.nix
+++ test.nix
@@ -10,7 +10,7 @@
       export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
       python3 -m http.server 3000 &> http-server.log &
       trap "kill %1" EXIT
-      npm install @playwright/test@1.52.0
-      npx playwright@1.52.0 test
+      npm install @playwright/test@1.56.1
+      npx playwright@1.56.1 test
     '';
 }
